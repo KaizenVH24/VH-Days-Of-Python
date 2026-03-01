35d03a4cba0952aa87db242261a4aae5f863d4c7
Vinay Hulsurkar <186893860+KaizenVH24@users.noreply.github.com>
2026-03-01 11:20:23 +0530

    Day-02

---
+++ Day-02.py
@@ -0,0 +1,64 @@
+# Variables in Python 
+
+first_name = 'Vinay'
+last_name = 'VH24'
+country = 'India'
+city = 'New Delhi'
+age = 22
+is_married = False
+skills = ['Python', 'Data Structures', 'Machine Learning', 'Big Data', 'Web Development']
+person_info = {
+    'firstname': 'Vinay',
+    'lastname': 'VH24',
+    'country': 'India',
+    'city': 'New Delhi',
+    'education': 'Computer Engineering',
+    'domain_interest': 'Data Science & Big Data',
+    'internship': 'Mainflow Services and Technologies Pvt. Ltd.'
+}
+
+# Printing the values stored in the variables
+
+print('First name:', first_name)
+print('First name length:', len(first_name))
+print('Last name:', last_name)
+print('Last name length:', len(last_name))
+print('Country:', country)
+print('City:', city)
+print('Age:', age)
+print('Married:', is_married)
+print('Skills:', skills)
+print('Person information:', person_info)
+
+# Declaring multiple variables in one line
+
+first_name, last_name, country, age, is_married = 'Vinay', 'VH24', 'India', 21, False
+
+print(first_name, last_name, country, age, is_married)
+print('First name:', first_name)
+print('Last name:', last_name)
+print('Country:', country)
+print('Age:', age)
+print('Married:', is_married)
+
+# ------------------------------------------------------------------------------
+
+# Output
+
+First name: Vinay
+First name length: 5
+Last name: VH24
+Last name length: 4
+Country: India
+City: New Delhi
+Age: 22
+Married: False
+Skills: ['Python', 'Data Structures', 'Machine Learning', 'Big Data', 'Web Development']
+Person information: {'firstname': 'Vinay', 'lastname': 'VH24', 'country': 'India', 'city': 'New Delhi', 'education': 'Computer Engineering', 'domain_interest': 'Data Science & Big Data', 'internship': 'Mainflow Services and Technologies Pvt. Ltd.'}
+Vinay VH24 India 21 False
+First name: Vinay
+Last name: VH24
+Country: India
+Age: 21
+Married: False
+
